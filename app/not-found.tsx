4b0f77d94a8e1a52d542a0d6adbe59a2f1226540
Mern-Tapas <cyb6261452510@gmail.com>
2023-10-16 15:57:52 +0530

    deploy

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,7 +11,7 @@ function notfound() {
           <ImageBox className='' height={500} width={500} src='/stock/images/notfound.jpg' alt='page-not-found' />
         </div>
         <h1 className='text-center font-bold text-2xl mb-2'>Page Not Found</h1>
-        <p className='text-sm text-gray-400 text-center '>We're sorry. thc page vou requested could not be found
+        <p className='text-sm text-gray-400 text-center '>We're sorry thc page vou requested could not be found
           <br /> piease go back to the hotnepage</p>
         <Link href='/' className='p-2 px-6 bg-blue-600 text-white rounded-full block w-fit mt-2 mx-auto'>Back to Home</Link>
       </div>
