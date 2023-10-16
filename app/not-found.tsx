5dfdba637f4058fe1ec48a5b4ddd72cc0ed730e5
Mern-Tapas <cyb6261452510@gmail.com>
2023-10-16 16:01:57 +0530

    deploy

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -12,7 +12,7 @@ function notfound() {
         </div>
         <h1 className='text-center font-bold text-2xl mb-2'>Page Not Found</h1>
         <p className='text-sm text-gray-400 text-center '>Were sorry thc page vou requested could not be found
-          <br /> piease go back to the hotnepage</p>
+          <br /> please go back to the hotnepage</p>
         <Link href='/' className='p-2 px-6 bg-blue-600 text-white rounded-full block w-fit mt-2 mx-auto'>Back to Home</Link>
       </div>
     </div>
