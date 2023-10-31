251a903553571c733d47db263cbd6060c135f809
Mern-Tapas <cyb6261452510@gmail.com>
2023-10-31 16:32:47 +0530

    udpate from office

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -20,7 +20,7 @@ function ContactForm() {
     })
 
 
-    const inputhandler = (e) => {
+    const inputhandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target
 
         setMessages((pre) => {
@@ -57,7 +57,7 @@ function ContactForm() {
             <div className="p-2 w-full">
                 <div className="relative">
                     <label className="leading-7 text-sm text-gray-600">Message</label>
-                    <textarea id="message" onChange={inputhandler} name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                    {/* <textarea value={messages.massage} onChange={inputhandler} name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea> */}
                 </div>
             </div>
             <div className="p-2 w-full">
