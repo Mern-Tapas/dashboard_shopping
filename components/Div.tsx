cb1f65d0686a748b59f1fef7b34aa55342f7305c
Mern-Tapas <cyb6261452510@Gmail.com>
2023-11-15 23:37:32 +0530

    on

--- components/Div.tsx
+++ components/Div.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Div({ children, className }: { children: React.ReactNode, className: string }) {
     return (
-        <div className={`${className} bg-white rounded-md border-slate-200 dark:bg-black dark:border-slate-800 `}>
+        <div className={`${className} bg-white rounded-md dark:bg-black border-slate-200 dark:border-slate-800`}>
             {children}
         </div>
     )
